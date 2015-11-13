aefab63da79a35b55dc37bcfff281dbe6fb99d12
ORI <ori1016@hotmail.com>
2015-11-13 17:04:11 +0900

    Initial Commit

---
+++ LinuxProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LinuxProject
+//
+//  Created by 임태근 on 2015. 11. 13..
+//  Copyright © 2015년 closet. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
